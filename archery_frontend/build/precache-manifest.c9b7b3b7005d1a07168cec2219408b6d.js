b2b426ac374a9c30de54b1e1bc74503642aa274a
sschwei1 <24sebastian05@gmail.com>
2020-10-18 00:24:22 +0200

    remove /build from gitignore
    so build can be created on dev machine and transported to host machine via git

---
+++ archery_frontend/build/precache-manifest.c9b7b3b7005d1a07168cec2219408b6d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285a6e96a4bce5de589660f90827cc3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58c58d7fb7105dc4a378",
+    "url": "/static/css/main.e5939a50.chunk.css"
+  },
+  {
+    "revision": "34f516b54970563bf17e",
+    "url": "/static/js/2.5f06c6e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f06c6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c58d7fb7105dc4a378",
+    "url": "/static/js/main.b959bb31.chunk.js"
+  },
+  {
+    "revision": "c858de2f668a74a29603",
+    "url": "/static/js/runtime-main.c6d0901d.js"
+  },
+  {
+    "revision": "bfef1d6d956d796fe783ebf50dfb7a89",
+    "url": "/static/media/BlackBg.bfef1d6d.mp4"
+  },
+  {
+    "revision": "742cd5152be599bb6e89d49cbf34e338",
+    "url": "/static/media/archery_no_bg_test.742cd515.png"
+  },
+  {
+    "revision": "1df1e222cd61b365d05fe3a935fb9329",
+    "url": "/static/media/klar_profile_medium.1df1e222.png"
+  },
+  {
+    "revision": "9080616cdc05d0e934df46a3dd9be050",
+    "url": "/static/media/klar_profile_small.9080616c.png"
+  },
+  {
+    "revision": "249582fb424c1b4c5bc5c1e9d182a150",
+    "url": "/static/media/logo_transparent_no_name_swapped_colors.249582fb.png"
+  },
+  {
+    "revision": "3ca63d61f9ca2acecc2f2bd4822bfc80",
+    "url": "/static/media/setup_guide_test3.3ca63d61.png"
+  },
+  {
+    "revision": "7d68ac3f428a9d3752e1f01c9710e603",
+    "url": "/static/media/sswe_profile_medium.7d68ac3f.png"
+  },
+  {
+    "revision": "fd18592b1b1e99e00ff8271c78f66bac",
+    "url": "/static/media/sswe_profile_small.fd18592b.png"
+  }
+]);
