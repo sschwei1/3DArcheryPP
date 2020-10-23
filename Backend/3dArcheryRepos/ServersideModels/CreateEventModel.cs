73c01fad0a5c7dedf3126731ab9badab01e8ed35
Kevin.Larson <ke.la@outlook.at>
2020-10-23 10:05:11 +0200

    add create event model

---
+++ Backend/3dArcheryRepos/ServersideModels/CreateEventModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3dArcheryRepos.ServersideModels
+{
+    class CreateEventModel
+    {
+        public string Name  { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int EventCode { get; set; }
+        public int TrackId { get; set; }
+        public int CountTypeId { get; set; }
+
+    }
+}
