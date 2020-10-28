b5d6d3949c568165b01650f3601c981167fb63ef
sschwei1 <24sebastian05@gmail.com>
2020-10-28 09:43:03 +0100

    update api request

--- archery_frontend/src/apiRequests/trackRequests.js
+++ archery_frontend/src/apiRequests/trackRequests.js
@@ -1,7 +1,7 @@
 import { CheckResponse } from "./errorHandling";
 
 export const GetTracks = (from, to, filterName = '', filterLocation = '') => {  
-  return fetch(`http://pp.sswe.me/track/GetTrackFiltered?filterFrom=${from}&filterTo=${to}&filterName=${filterName}&filterLocation=${filterLocation}`)
+  return fetch(`http://pp.sswe.me/api/track/GetTrackFiltered?filterFrom=${from}&filterTo=${to}&filterName=${filterName}&filterLocation=${filterLocation}`)
   .then(res => res.json())
   .then(res => CheckResponse(res));
 };
